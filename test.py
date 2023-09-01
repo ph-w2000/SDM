fix iou calculation error of 0

test.py
@@ -99,7 +99,7 @@ def calculate_iou(array1, array2):
         if torch.sum(union) != 0:
             iou = torch.sum(intersection) / torch.sum(union)
         else:
-            iou =  torch.zeros(1).cuda()
+            iou =  torch.zeros(1).squeeze().cuda()
         ious.append(iou)
     ious = torch.stack(ious)
     return torch.mean(ious, dim=0)

train.py
@@ -116,7 +116,7 @@ def calculate_iou(array1, array2):
         if torch.sum(union) != 0:
             iou = torch.sum(intersection) / torch.sum(union)
         else:
-            iou = torch.zeros(1).cuda()
+            iou = torch.zeros(1).squeeze().cuda()
         ious.append(iou)
 
     ious = torch.stack(ious)
@@ -412,6 +412,6 @@ if __name__ == "__main__":
         if not os.path.isdir(args.save_path): os.mkdir(args.save_path)
         if not os.path.isdir(DiffConf.training.ckpt_path): os.mkdir(DiffConf.training.ckpt_path)
 
-    # DiffConf.ckpt = "checkpoints/pidm_deepfashion/last_epoch_41.pt"
+    # DiffConf.ckpt = "checkpoints/pidm_deepfashion/last.pt"
 
     main(settings = [args, DiffConf, DataConf], EXP_NAME = args.exp_name)